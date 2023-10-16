be7b679a3603d71273823ece80d882590525cb7a
brittadam <britt.adam@hotmail.com>
2023-10-16 22:15:40 +0200

    feat: add code to class weather

---
+++ classes/Weather.js
@@ -0,0 +1,37 @@
+export default class Weather {
+    constructor() {
+      this.getLocation();
+      this.lat;
+      this.lng;
+    }
+  
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(
+        this.gotLocation.bind(this), 
+        this.errorLocation.bind(this)
+        );
+    }
+  
+    gotLocation(result){
+      this.lat = result.coords.latitude;
+      this.lng = result.coords.longitude;
+      this.getWeather();
+    }
+  
+    getWeather(){
+      //https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely
+      let url = `https://api.weatherbit.io/v2.0/current?lat=${this.lat}&lon=${this.lng}&key=9117630991164d73894b20e2d041d492&include=minutely`
+      fetch(url)
+      .then(response => {
+        return response.json();
+      }).then(data => {
+          document.querySelector("#weather").innerHTML = data.data[0].temp;
+      }).catch(err => {
+        console.log(err);
+      });
+    };
+  
+    errorLocation(err){
+      console.log(err);
+    }
+  }
